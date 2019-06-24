157c95db94303ccd11d445fa5d415b16640c5426
Megan <meganle@utexas.edu>
2019-06-24 12:34:48 -0500

    moved getGlobalMinima to utilities

--- potentials.cpp
+++ potentials.cpp
@@ -4,6 +4,9 @@
 const double SIGMA = 1.0;
 const double EPSILON = 1.0;
 const double FORCE_DAMPING = .75;
+const double EQUILIBRIUM_BOND_LENGTH = 1.2;
+const double WELL_DEPTH = -999;
+const double ALPHA = -999;
 
 double getLennardJonesEnergy(double distance) {
   return 4 * EPSILON * (pow(SIGMA / distance, 12) - pow(SIGMA / distance, 6));
@@ -12,4 +15,12 @@ double getLennardJonesEnergy(double distance) {
 double getLennardJonesForce(double distance) {
   return -4 * FORCE_DAMPING * EPSILON *
          ((-12 * pow(SIGMA / distance, 13)) - (-6 * pow(SIGMA / distance, 7)));
+}
+
+double getMorseEnergy(double distance){
+  return WELL_DEPTH * pow(1 - exp(-1 * ALPHA * (distance - EQUILIBRIUM_BOND_LENGTH)), 2);
+}
+
+double getMorseForce(double distance){
+  return 0.;
 }
--- utility.h
+++ utility.h
@@ -1,5 +1,10 @@
 #include <string>
 
+//checks if given file exists
+bool fileExists(const std::string &name);
+
+// Reads in global minimum from global_minima.txt
+double getGlobalMinima(int cluster_size);
+
 // checks if char array represents a number
 bool isNumber(char number[]);
-bool fileExists(const std::string &name);
