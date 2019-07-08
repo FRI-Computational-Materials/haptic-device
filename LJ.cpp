5e8fa48505124ccf752a7914be38df84801a3095
Alex C <acjarv11@gmail.com>
2019-07-08 12:15:56 -0500

    New logging system.

--- LJ.cpp
+++ LJ.cpp
@@ -897,16 +897,35 @@ void keyCallback(GLFWwindow *a_window, int a_key, int a_scancode, int a_action,
   } else if (a_key == GLFW_KEY_SPACE) {  // freeze simulation
     freezeAtoms = !freezeAtoms;
   } else if (a_key == GLFW_KEY_C) {  // save atoms to con file
-    
     ofstream writeFile;
+    string dir1 = "./log/";
+    struct stat buffer;
+    if (stat(dir1.c_str(), &buffer) != 0) { // Check if log directory exists
+      char cstr[dir1.size() + 1];
+      strcpy(cstr, dir1.c_str());
+      mkdir(cstr, 0777);
+    }
 
-    // prevent overwriting .con files
+    // Find local date
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    int year = 1900 + ltm -> tm_year;
+    int month = 1 + ltm -> tm_mon;
+    int day = ltm -> tm_mday;
+    string date = to_string(month) + "-" + to_string(day) + "-" + to_string(year);
+    string dir2 = dir1 + date + "/";
+    if (stat(dir2.c_str(), &buffer) != 0) { // Check if date directory exists
+      char cstr[dir2.size() + 1];
+      strcpy(cstr, dir2.c_str());
+      mkdir(cstr, 0777);
+    }
+    // Prevent overwriting .con files
     int index = 0;
-    while (fileExists("./log/atoms" + to_string(index) + ".con")) {
+    while (fileExists(dir2 + "atoms" + to_string(index) + ".con")) {
       index++;
     }
-    writeToCon("./log/atoms" + to_string(index) + ".con");
-    cout << "LOGGED: " << "atoms" + to_string(index) + ".con" << endl;
+    writeToCon(dir2 + "atoms" + to_string(index) + ".con");
+cout << "LOGGED AT " + date + " atoms" + to_string(index) + ".con" << endl;
   } else if (a_key == GLFW_KEY_A) {
     // anchor all atoms while maintaining control
     for (auto i{0}; i < spheres.size(); i++) {
